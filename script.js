c47f382ecb0ea26282d5b0f7d452dcbb6f1abf31
ayushrp147 <174237647+ayushrp147@users.noreply.github.com>
2024-07-20 22:22:01 +0530

    Add logic for clear and decimal operation

--- script.js
+++ script.js
@@ -1,86 +1,3 @@
-// function add(number1, number2) {
-//     return number1 + number2;
-// }
-// function subtract(number1, number2) {
-//     return number1 - number2;
-// }
-// function multiply(number1, number2) {
-//     return number1 * number2;
-// }
-// function divide(number1, number2) {
-//     return number1 / number2;
-// }
-// let firstNumber, secondNumber, operator;
-// function operate(firstNumber, operator, secondNumber) {
-//     switch (operator) {
-//         case '+':
-//             return add(firstNumber, secondNumber);
-//         case '-':
-//             return subtract(firstNumber, secondNumber);
-//         case '*':
-//             return multiply(firstNumber, secondNumber);
-//         case '/':
-//             return divide(firstNumber, secondNumber);
-//     }
-// }
-// let resultDisplayed = false;
-// const numbers = document.querySelectorAll(".number");
-// let display = document.querySelector(".display")
-// const numbersArray = Array.from(numbers);
-// numbersArray.forEach((number) => {
-//     number.addEventListener("click", () => {
-//         const value = number.textContent;
-//         if (resultDisplayed) {
-//             display.textContent = value;
-//             resultDisplayed = false;
-//         }
-//         else {
-//             if (display.textContent === "0") {
-//                 display.textContent = value;
-//             }
-//             else {
-//                 display.textContent += value;
-//             }
-//         }
-//     });
-// });
-// const operatorButton = document.querySelectorAll(".operator")
-// const operatorArray = Array.from(operatorButton);
-// operatorArray.forEach((op) => {
-//     op.addEventListener("click", () => {
-//         if (firstNumber && operator && !resultDisplayed) {
-//             secondNumber = parseInt(display.textContent);
-//             console.log(secondNumber);
-//             const result=operate(firstNumber,operator,secondNumber);
-//             display.textContent=result;
-//             firstNumber=result.toString();
-//             // secondNumber="";
-//             resultDisplayed=false;
-//         }
-//         else{
-//             firstNumber=+display.textContent;
-//             console.log(typeof firstNumber);
-//             operator = op.textContent;
-//             console.log(`operator ${operator}: ${typeof operator}`);
-//             console.log(operator)
-//             display.textContent = "0";
-//         }
-//     });
-// });
-// const equal = document.querySelector(".equal");
-// equal.addEventListener("click", () => {
-//     if (firstNumber && operator) {
-//         secondNumber = +display.textContent;
-//         console.log(typeof secondNumber);
-//         const finalValue = operate(firstNumber, operator, secondNumber);
-//         firstNumber = finalValue;
-//         display.textContent = finalValue.toString();
-//         console.log(finalValue);
-//         // secondNumber = "";
-//         // operator = "";
-//         resultDisplayed = true;
-//     }
-// });
 function add(number1, number2) {
     console.log(`Add: ${number1} + ${number2} = ${number1 + number2}`);
     return number1 + number2;
@@ -170,7 +87,7 @@ equal.addEventListener("click", () => {
         secondNumber = parseFloat(display.textContent);
         console.log(`Equal Click: firstNumber=${firstNumber}, operator=${operator}, secondNumber=${secondNumber}`);
         const finalValue = operate(firstNumber, operator, secondNumber);
-        display.textContent = finalValue;
+        display.textContent = finalValue.toFixed(2);
         firstNumber = parseFloat(finalValue);
         secondNumber = null;
         operator = null;
@@ -182,3 +99,20 @@ clear.addEventListener("click",()=>{
     location.reload();
 });
 
+window.addEventListener("keydown",checkKeyPressed,false);
+function checkKeyPressed(e){
+    if(e.keyCode===8){
+        let text=display.textContent;
+        text=text.slice(0,text.length-1);
+        display.textContent=text;
+        console.log(text);
+    }
+}
+
+const dot=document.querySelector(".dot");
+dot.addEventListener("click",() =>{
+    let text=display.textContent;
+    text+=".";
+    display.textContent=text;
+    console.log(text);
+});
